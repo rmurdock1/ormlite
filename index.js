6b762913a10c11f38d6076a9aa1704f07c956b34
rmurdock1 <rmurdoc2047@gmail.com>
2018-01-11 12:00:12 -0500

    almost done just need to render findall elements to the front end

--- index.js
+++ index.js
@@ -1,136 +1,55 @@
-const express = require('express');
-const parseurl = require('parseurl');
-const bodyParser = require('body-Parser');
-const path = require('path');
-const expressValidator = require('express-validator');
-const session = require('express-session');
-const mustacheExpress = require('mustache-express');
-const models = require('./models');
-models.sequelize.sync();
-
-const pg = require('pg');
-
-const app = express();
+var express = require('express');
+var pg = require('pg');
+var port = process.env.PORT || 4000;
+var bodyParser = require('body-Parser');
+var mustacheExpress = require('mustache-express');
+var orm = require('./orm');
+var app = express();
 
 
+var User = new orm('users','postgres://postgres:root@localhost/orm');
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', './views')
-
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(expressValidator());
 
 
-app.use(session({
-  secret: 'key',
-  resave: false,
-  saveUninitialized: true
-}));
-
-app.get('/', function(req, res) {
-  res.render('index')
-})
-
-app.get('/signup', function(req, res) {
-  res.render('signup');
+app.get('/', function(req, res){
+  User.getAll(function(data) {
+    res.render('index',{
+      data: data
+    });
+  });
 });
 
-app.get('/login', function(req, res) {
-  res.render('login');
+app.get('/find', function(req, res){
+  User.findById(req.params.id,function(data){
+    console.log(data);
+    res.render('find',{
+       data: data [1]
+    });
+  });
+});
+// app.get('/id/:id', function (req, res) {
+//   User.findById(req.params.id, function (data) {
+//     if (!err) {
+//       res.render('/find', {});
+//     } else {
+//       return console.log(err);
+//     }
+//   });
+// });
+
+app.get('*', function(req, res){
+  res.status(404).send('page not found!');
 });
 
-
-app.get('/login', function(req, res) {
-  if (req.session && req.session.authenticated) {
-    var user = models.user.findOne({
-      where: {
-        username: req.session.username,
-        password: req.session.password
-      }
-    }).then(function(user) {
-      if (user) {
-        req.session.username = req.body.username;
-        req.session.userId = user.dataValues.id;
-        let username = req.session.username;
-        let userid = req.session.userId;
-        res.render('index', {
-          user: user
-        });
-      }
-    })
-  } else {
-    res.redirect('/home')
-  }
-})
-
-
-app.post('/login', function(req, res) {
-  let username = req.body.username;
-  let password = req.body.password;
-
-  models.user.findOne({
-    where: {
-      username: username,
-      password: password
-    }
-  }).then(user => {
-    if (user.password == password) {
-      req.session.username = username;
-      req.session.userId = user.dataValues.id;
-      req.session.authenticated = true;
-      console.log(req.session);
-
-      res.redirect('/home');
-    } else {
-      res.redirect('/login');
-      console.log("Incorrect Password", req.session)
-    }
-  })
-})
-
-
-
-app.post('/signup', function(req, res) {
-  const user = models.user.build({
-    name: req.body.name,
-    email: req.body.email,
-    username: req.body.username,
-    password: req.body.password
-  })
-  console.log(req.body);
-
-  user.save().then(function(user) {
-    req.username = user.username;
-    req.session.authenticated = true;
-    res.redirect('/login')
-    console.log(req.session);
-  })
-
-
-});
-
-app.get('/home', function(req, res) {
-  models.user.findAll().then(function(posts) {
-    res.render('home', {
-      name: req.session.username
-    })
-  })
-});
-
-app.get('/logout', function(req, res) {
-  req.session.destroy(function(err) {})
-  res.render('index');
-  console.log(req.session);
-});
-
-
-var port = process.env.PORT || 5000;
 
 app.listen(port, function(){
 	console.log("Listening on PORT " + port);
---
+++ orm.js
@@ -0,0 +1,88 @@
+var Sequelize = require('sequelize');
+
+class Table{
+  constructor(tableName, connectionString){
+    this.tableName = tableName;
+    this.sequelize = this.initiate(connectionString);
+    this.table = this.createTable(this.tableName);
+    this.authenticate = this.authenticate();
+    this.insertIntoTable = this.insertIntoTable(this.tableName);
+  }
+  initiate(connectionString){
+    return new Sequelize(connectionString);
+  }
+
+  authenticate(){
+    this.table.sync();
+      this.sequelize.authenticate()
+    .then(() => {
+      console.log('Connection to users table has been established successfully.');
+
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
+  }
+
+  createTable(tablename) {
+    console.log('Database updated with table name ' + tablename);
+    return this.sequelize.define(tablename, {
+      firstname: Sequelize.STRING,
+      lastname: Sequelize.STRING
+    }, {
+      freezeTableName: true
+    });
+  }
+
+insertIntoTable(tablename) {
+  var tableRef = this.table;
+  this.table.sync().then(function() {
+    console.log('Adding Members to table' + tablename);
+    tableRef.bulkCreate([{
+        firstname: 'foo',
+        lastname: '123'
+      },
+      {
+        firstname: 'test',
+        lastname: 'test'
+      },
+      {
+        firstname: 'hry',
+        lastname: 'buddy'
+      },
+    ], {
+      validate: true
+    }).catch(errors => {
+      console.log(errors)
+      //callback();
+    })
+  });
+}
+
+
+  getAll(cb){
+    this.table.findAll().then(function(rows) {
+      var data = [];
+      for(var i = 0; i < rows.length; i++) {
+        data.push(rows[i].dataValues);
+      }
+      return cb(data);
+    });
+  }
+
+  findById(id, cb){
+    this.table.findAll({
+      where: {
+        id: id
+      }
+    }).then(function(rows) {
+       var data = [];
+       for(var i = 0; i < rows.length; i++) {
+         data.push(rows[i].dataValues);
+       }
+       return cb(data);
+    });
+  }
+}
+
+module.exports = Table;
